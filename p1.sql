16fe7b796916964b2fd13aba0988ff9834b6e480
Soham4410 <sohampatel041020066@gmail.com>
2025-06-25 15:42:50 +0530

    Add files via upload

---
+++ p1.sql
@@ -0,0 +1,12 @@
+-- create database d2da;
+use d2da;
+-- create table Student(id int, st_name text,age int);
+-- insert into Student(id,st_name,age) value(1,'soham',23);
+select * from Student;
+-- insert into Student(id,st_name,age) values
+-- (2,'amit',35),
+-- (3,'pradip',55),
+-- (4,'mark',34);
+-- insert into Student(id,st_name) values
+-- (11,'sumit'),
+-- (22,'bhavin');
